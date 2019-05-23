158affe527ad32da29f2a5ff38e2fdeeafdd9c17
JuandaAtilano <50937344+JuandaAtilano@users.noreply.github.com>
2019-05-22 22:09:27 -0500

    Add files via upload

---
+++ CONEXION.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace UTILIDADES
+{
+    public class CONEXION
+    {
+        public string CadenaConexion()
+        {
+            return ConfigurationManager.ConnectionStrings["Conexion"].ToString();
+        }
+
+    }
+
+}
